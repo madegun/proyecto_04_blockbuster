1908edc82e0dd8f33c3c2da0fc83c75c7f7b8b9e
biorubenfs <biorubenfs@hotmail.com>
2021-04-01 16:32:29 +0200

    Password implemented in /auth endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import user from '../models/user.model.js';
+import Bcryp from 'bcrypt';
 
 // This controller gives a token to the user if this one has been registered previously
 export const authController = {
@@ -7,23 +8,32 @@ export const authController = {
 
         const username = req.body.username
         const email = req.body.email;
+        const password = req.body.password;
 
         // Generating JWT IF user is ALREADY registered.
         // Note that we can't use checkUser middleware in this part because we have not yet generated the token.
         try {
             const queryUser = await user.findOne({ email: email });
-            if (!queryUser) {
-                res.send("User is not registered");
-            } else if (email === queryUser.email) {
-                const payload = {
-                    username: username,
-                    email: email
-                }
-                const secret = process.env.SECRET;
-                const token = jwt.sign(payload, secret);
 
-                res.json({ token });
+            console.log(queryUser.password);
+            console.log(password);
+
+            if (!queryUser) {
+                return res.send({ message: "User is not registered (email)" });
             }
+            // Checking password
+            if (!Bcryp.compareSync(password, queryUser.password)) {
+                return res.send({ message: "Incorrect password" });
+            };
+
+            const payload = {
+                username: username,
+                email: email
+            }
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret);
+
+            res.json({ token });
         } catch (error) {
             console.log(error);
             res.send("Something was wrong");
